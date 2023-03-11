465f22ca34388c619612bc5638aee7008ff994ea
Daahir Ibraahim <Daahir.ibraahim89@gmail.com>
2023-03-11 20:57:36 +0100

    App.jsx comment

--- my-react-website/src/App.jsx
+++ my-react-website/src/App.jsx
@@ -17,3 +17,47 @@ function App() {
 }
 
 export default App;
+
+/*
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Booking from "./components/Booking";
+import Contact from "./components/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <nav className="navbar">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/booking" className="nav-link">
+                Book a Table
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Contact" className="nav-link">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/booking" component={Booking} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+*/
